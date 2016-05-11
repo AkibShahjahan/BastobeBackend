f430f0953665084f12939c689b4e9154c5166b6a
AkibShahjahan <akibshahjahan@gmail.com>
2016-05-10 23:12:34 -0400

    Add middleware

--- server.js
+++ server.js
@@ -107,35 +107,6 @@ app.delete("/users/:id", function(req, res){
 	});
 });
 
-// passport.use(new FacebookTokenStrategy({
-//         clientID: '1541173709535196',
-//         clientSecret: '9cfe46bb94433454454a281b0b4aba4d',
-//     }, function(accessToken, refreshToken, profile, done) {
-//         console.log(profile);
-//                     // Do stuff with the profile. You're already getting authenticated data so there's no need to double check anything! For example
-      //    User.findOne({'fbID': profile.id}, function(err, user){
-    		// 	if(err)
-    		// 		return done(err);
-    		// 	if(user)
-    		// 		return done(null, user);
-    		// 	else {
-    		// 		var newUser = new User();
-    		// 		newUser.fbID = profile.id;
-    		// 		newUser.fbToken = accessToken;
-    		// 		newUser.firstName = profile.name.givenName;
-    		// 		newUser.lastName = profile.name.familyName;
-    		// 		newUser.email = profile.displayName;
-    		// 		newUser.points = 0;
-    		// 		newUser.save(function(err){
-    		// 			if(err)
-    		// 				throw err;
-    		// 			return done(null, newUser);
-    		// 		})
-    		// 		console.log(profile);
-    		// 	}
-    		// });
-//     });
-// );
 
 passport.use(new FacebookTokenStrategy({
         clientID: '1541173709535196',
@@ -180,6 +151,29 @@ app.post('/login/facebook', passport.authenticate('facebook-token', {session: fa
     return res.json({status: 'OK'});
 });
 
+
+var middlewareObj = {};
+middlewareObj.isLoggedIn = function(req, res, next){
+	if(req.isAuthenticated())
+	{
+		return next();
+	}
+	res.json({error: "You are unauthenticated."});
+}
+
+app.get('/points/:id', middlewareObj.isLoggedIn, function(req, res){
+	User.findOne({'fbID': profile.id}, function(err, user){
+		if(err)
+		{
+			console.log(err);
+			res.json({error: "Finding failed."});
+		}
+		else
+		{
+			res.send(user.points);
+		}
+	});
+});
 
 // app.listen(3000, function () {
 //   console.log("Bastobe server listening on port 3000!");
