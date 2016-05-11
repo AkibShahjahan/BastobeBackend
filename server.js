dc726aebe8e6a66b4c4c31b7fbc6e6a5284ce59a
AkibShahjahan <akibshahjahan@gmail.com>
2016-05-11 16:03:55 -0400

    dsf

--- server.js
+++ server.js
@@ -34,9 +34,8 @@ require("./config/passport")(passport);
 // }));
 
 // // Init passport authentication 
- app.use(passport.initialize());
+ //app.use(passport.initialize());
 // // persistent login sessions 
- app.use(passport.session());
 
 //================================================
 
