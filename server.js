6f44d89464a07a92f634698da4b7e05aa6d64d6e
AkibShahjahan <akibshahjahan@gmail.com>
2016-05-10 20:04:08 -0400

    Try to add fb authentication with token

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var passport = require('passport');
-var FacebookTokenStrategy = require('passport-facebook-token').Strategy;
+var FacebookTokenStrategy = require('passport-facebook-token');
 var http = require('http');
 var url = require('url')
 var bodyParser = require("body-parser");
@@ -107,13 +107,43 @@ app.delete("/users/:id", function(req, res){
 	});
 });
 
+// passport.use(new FacebookTokenStrategy({
+//         clientID: '1541173709535196',
+//         clientSecret: '9cfe46bb94433454454a281b0b4aba4d',
+//     }, function(accessToken, refreshToken, profile, done) {
+//         console.log(profile);
+//                     // Do stuff with the profile. You're already getting authenticated data so there's no need to double check anything! For example
+      //    User.findOne({'fbID': profile.id}, function(err, user){
+    		// 	if(err)
+    		// 		return done(err);
+    		// 	if(user)
+    		// 		return done(null, user);
+    		// 	else {
+    		// 		var newUser = new User();
+    		// 		newUser.fbID = profile.id;
+    		// 		newUser.fbToken = accessToken;
+    		// 		newUser.firstName = profile.name.givenName;
+    		// 		newUser.lastName = profile.name.familyName;
+    		// 		newUser.email = profile.displayName;
+    		// 		newUser.points = 0;
+    		// 		newUser.save(function(err){
+    		// 			if(err)
+    		// 				throw err;
+    		// 			return done(null, newUser);
+    		// 		})
+    		// 		console.log(profile);
+    		// 	}
+    		// });
+//     });
+// );
+
 passport.use(new FacebookTokenStrategy({
         clientID: '1541173709535196',
         clientSecret: '9cfe46bb94433454454a281b0b4aba4d',
     }, function(accessToken, refreshToken, profile, done) {
         console.log(profile);
                     // Do stuff with the profile. You're already getting authenticated data so there's no need to double check anything! For example
-         User.findOne({'fbID': profile.id}, function(err, user){
+                    User.findOne({'fbID': profile.id}, function(err, user){
     			if(err)
     				return done(err);
     			if(user)
@@ -137,7 +167,7 @@ passport.use(new FacebookTokenStrategy({
     })
 );
 
-d app.post('/login/facebook', passport.authenticate('facebook-token', {session: false}), function(req, res) {
+app.post('/login/facebook', passport.authenticate('facebook-token', {session: false}), function(req, res) {
     // Congratulations, you're authenticated!
     return res.json({status: 'OK'});
 });
