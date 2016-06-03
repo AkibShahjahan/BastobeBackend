998ddd713de6e874ff2fbbdddeb39dc7a1471631
AkibShahjahan <akibshahjahan@gmail.com>
2016-06-02 20:36:21 -0400

    dfasf

--- config/passport.js
+++ config/passport.js
@@ -10,34 +10,35 @@ module.exports = function(passport){
     }, function(accessToken, refreshToken, profile, done) {
         console.log(profile);
         // ADDING status code here screws everythign up
-            User.findOne({'facebook.id': profile.id}, function(err, user){
-    			if(err)
-    			{
-    				//res.status(500);
-    				return done(err);
-    			}
-    			if(user)
-    			{
-    				//res.status(200);
-    				return done(null, user);
-    			}
-    			else {
-    				var newUser = new User();
-    				newUser.facebook.id = profile.id;
-    				newUser.facebook.token = accessToken;
-    				newUser.facebook.firstName = profile.name.givenName;
-    				newUser.facebook.lastName = profile.name.familyName;
-    				newUser.facebook.email = profile.displayName;
-    				newUser.points = 1;
-    				newUser.save(function(err){
-    					if(err){
-    						//res.status(500);
-    						throw err;
-    					}
-    					//res.status(201)
-    					return done(null, newUser);
-    				})
-    			}
+          User.findOne({'facebook.id': profile.id}, function(err, user){
+	    			if(err)
+	    			{
+	    				//res.status(500);
+	    				return done(err);
+	    			}
+	    			if(user)
+	    			{
+	    				//res.status(200);
+	    				return done(null, user);
+	    			}
+	    			else {
+	    				var newUser = new User();
+	    				newUser.facebook.id = profile.id;
+	    				newUser.facebook.token = accessToken;
+	    				newUser.facebook.firstName = profile.name.givenName;
+	    				newUser.facebook.lastName = profile.name.familyName;
+	    				newUser.facebook.email = profile.displayName;
+	    				newUser.points = 1;
+							newUser.receivedMedias = [];
+	    				newUser.save(function(err){
+	    					if(err){
+	    						//res.status(500);
+	    						throw err;
+	    					}
+	    					//res.status(201)
+	    					return done(null, newUser);
+	    				})
+	    			}
     		});
     	})
 	);
