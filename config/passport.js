0ba0fd7eb9433616b1accb46810ffe36d1ef3de1
AkibShahjahan <akibshahjahan@gmail.com>
2016-06-23 20:17:00 -0400

    df

--- config/passport.js
+++ config/passport.js
@@ -42,7 +42,7 @@ module.exports = function(passport){
 										userId: newUserId,
 										likeRecord: [],
 										spreadRecord: [],
-										commentRecord: []
+										commentRecord: [] 
 									};
 
 									UserRecord.create(newUserRecord, function(err, newRecordCreation) {
