11bc997112df23c70da033d55282b05d3d045223
AkibShahjahan <akibshahjahan@gmail.com>
2016-12-29 10:12:40 -0500

    Trying something out

--- controllers/medias.js
+++ controllers/medias.js
@@ -184,6 +184,7 @@ function globalRank(userId, req, res, preview) {
 }
 
 router.get("/rank/:x/:y/:userId", function(req, res){
+	console.log("Authentication Status: " + req.isAuthenticated());
 	var userId = req.params.userId;
 	var x = parseFloat(req.params.x);
 	var y = parseFloat(req.params.y);
